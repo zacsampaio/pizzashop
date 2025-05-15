5416ca8eb9c497bd0ca4f1a9c5fbf009d6715796
Isaac Sampaio <isaac.s.cruzz@gmail.com>
2025-05-14 22:25:42 -0300

    Integração API: Dados Perfil User

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -10,8 +10,21 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
 } from "@radix-ui/react-dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ["managed-restaurant"],
+    queryFn: getManagedRestaurant,
+  });
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -19,23 +32,23 @@ export function AccountMenu() {
           variant="outline"
           className="flex cursor-pointer items-center gap-2 select-none"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col gap-1">
-          <span>Isaac Sampaio</span>
+          <span>{profile?.name}</span>
           <span className="text-muted-foreground text-xs font-normal">
-            isaac@pizzashop.com.br
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="flex cursor-pointer items-center mb-2">
+        <DropdownMenuItem className="mb-2 flex cursor-pointer items-center">
           <Building className="mr-2 h-4 w-4" />
           <span>Perfil da Loja</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className="flex cursor-pointer items-center text-red-500 dark:text-red-400 mb-1">
+        <DropdownMenuItem className="mb-1 flex cursor-pointer items-center text-red-500 dark:text-red-400">
           <LogOut className="mr-2 h-4 w-4" />
           <span>Sair</span>
         </DropdownMenuItem>
--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,5 @@
 import { z } from 'zod';
 
-console.log(import.meta.env.VITE_API_URL)
-
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
 });
