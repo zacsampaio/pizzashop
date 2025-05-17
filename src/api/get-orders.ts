403f6c0e4d794a0c966b22daee5b818269aef36b
Isaac Sampaio <isaac.s.cruzz@gmail.com>
2025-05-17 15:34:04 -0300

    Integração com os filtros

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -2,6 +2,9 @@ import { api } from "@/lib/axios";
 
 export interface GetOrdersQuery {
   pageIndex: number | null;
+  orderId?: string | null;
+  customerName?: string | null;
+  status?: string | null;
 }
 
 interface GetOrdersResponse {
@@ -19,12 +22,20 @@ interface GetOrdersResponse {
   };
 }
 
-export async function getOrders({ pageIndex }: GetOrdersQuery) {
+export async function getOrders({
+  pageIndex,
+  orderId,
+  customerName,
+  status,
+}: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>("/orders", {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
-  })
+  });
 
-  return response.data
+  return response.data;
 }
